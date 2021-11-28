6763969ca6f6f553b1b2dab61e7e4a978c94c3f2
unknown <aalshanbayev@gmail.com>
2021-11-27 22:56:31 -0500

    alternative approach of calling state update function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -13,9 +13,15 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     // setEnteredTitle(event.target.value)
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // })
+    setUserInput((prevState)=> {
+      return {
+        ...prevState,
+        enteredTitle: event.target.value,
+      }
     })
   }
 
