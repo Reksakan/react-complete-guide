b3a50a5e7a1f517fb42dc6c1ecab425ab7db6844
unknown <aalshanbayev@gmail.com>
2021-11-28 02:14:06 -0500

    Filter inform that now items exist for certain year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,15 +11,24 @@ const Expenses = ({expenses}) => {
     setFilteredYear(selectedYear)
   }
 
-  const expenseBoxes = expenses.map(expense => {
-    return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>
+  const filteredExpenses = expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
   })
+
+  let expenseContent = <p>No expenses found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map(expense => {
+      return <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
+    })
+  }
+  
   
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {expenseBoxes}
+        {expenseContent}
       </Card>
     </div>
   )
