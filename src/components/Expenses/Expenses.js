6fd45cb2f25b14eebca57c6906b5c3cde9f07bb0
unknown <aalshanbayev@gmail.com>
2021-12-05 14:01:51 -0500

    bar component is realised

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
 const Expenses = ({expenses}) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -21,6 +22,7 @@ const Expenses = ({expenses}) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
